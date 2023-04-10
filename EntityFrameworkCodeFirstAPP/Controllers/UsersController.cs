b7d22b31425acd25096987ab484d16d994d02c34
Dell <Dell@SARWA>
2023-04-10 16:25:16 +0500

    Add project files.

---
+++ EntityFrameworkCodeFirstAPP/Controllers/UsersController.cs
@@ -0,0 +1,80 @@
+﻿using EntityFrameworkCodeFirstAPP.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace EntityFrameworkCodeFirstAPP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UsersController : ControllerBase
+    {
+        private readonly UserContext userContext;
+
+        public UsersController(UserContext userContext)
+        {
+            this.userContext = userContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllUsers()
+        {
+            var users = await userContext.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetUserById(int id)
+        {
+            var users = await userContext.Users.FindAsync(id);
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Ok(users);
+        }
+        [HttpPost]
+        public Users Post(Users users)
+        {
+
+            userContext.Users.Add(users);
+            userContext.SaveChanges();
+            return users;
+        }
+
+        [HttpPut("{id}")]
+        public Users Put(int id, Users user)
+        {
+            if (user != null)
+            {
+
+
+                userContext.Entry(user).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                userContext.SaveChanges();
+                return user;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteUser(int id)
+        {
+            Users user = userContext.Users.Where(x => x.ID == id).FirstOrDefault();
+            if (user != null)
+            {
+                userContext.Users.Remove(user);
+                userContext.SaveChanges();
+                return "User deleted";
+            }
+            else
+            {
+                return "No User Found";
+            }
+        }
+    }
+}
---
+++ EntityFrameworkCodeFirstAPP/Model/Users.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkCodeFirstAPP.Model
+{
+    public class Users
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }    
+        public string ContactNo { get; set; }
+    }
+}
